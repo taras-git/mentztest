7f2cab93b164bc1637b3192ad9d31ed35e7f5f81
Taras Tymchyshyn <tarast@livv.com>
2023-01-21 19:37:42 +0100

    Add isLoading to provider, refactore folder structure

---
+++ lib/widgets/locations_list.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mentztest/providers/locations_provider.dart';
+
+class LocationsList extends ConsumerWidget {
+  const LocationsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locations = ref.watch(locationsProvider).locations;
+    final isLoading = ref.watch(locationsProvider).isLoading;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 20,
+              ),
+              child: TextField(
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a search term',
+                ),
+                onSubmitted: (text) async {
+                  await ref
+                      .read(locationsProvider.notifier)
+                      .loadLocations(text);
+                },
+              ),
+            ),
+            if (isLoading)
+              const CircularProgressIndicator()
+            else
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: locations.length,
+                    itemBuilder: (
+                      context,
+                      index,
+                    ) {
+                      final loc = locations[index];
+                      return Text(loc.name!);
+                    },
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
