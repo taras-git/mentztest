899ff983b930c4722a0b584e70ef8be7e386c162
Taras Tymchyshyn <tarast@livv.com>
2023-01-23 10:52:33 +0100

    Add unit test dependancy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,5 +9,14 @@ void main() {
     await tester.pumpWidget(const LocationsSearchScreen());
     final messageFinder = find.text('Lcations locator');
     expect(messageFinder, findsOneWidget);
+
+    // Enter 'fd' into the TextField.
+    await tester.enterText(find.byType(TextFormField), 'fd');
+    // Tap the SearchIt button.
+    await tester.tap(find.byType(OutlinedButton));
+    // Rebuild the widget with the new item.
+    await tester.pump();
+    // Expect to find the item on screen.
+    expect(find.text('Fulda'), findsOneWidget);
   });
 }
